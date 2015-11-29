85352187d2c468499dda8114020146dfb4f89072
Christopher Rathnam <Rathnam.C@gmail.com>
2015-11-29 01:36:12 -0500

    File Update

--- retro2/ViewController.swift
+++ retro2/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var audioPlayer: AVAudioPlayer!
+    
+    var screenValue = ""
+    var leftValueString = ""
+    var rightValueString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Store numbers into strings
+        
+       
+        
+        
+        
+        //Locates the sound file. Set as constant
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        //Error catch if audio does not play
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: soundURL)
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Function Controls actions when number pad is pressed
+    @IBAction func numberPressed(btn: UIButton!) {
+        audioPlayer.play()
     }
-
 
 }
 
